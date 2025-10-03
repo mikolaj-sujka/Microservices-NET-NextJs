b167f610bacc0e69c7142286a6b4f241fa22fc9d
Mikołaj Sujka <mikolaj.sujka@qodeca.com>
2025-10-03 14:59:16 +0200

    Rename, working on SearchService.

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,34 +1,46 @@
 
-namespace SearchService
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.MapOpenApi();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        await DB.InitAsync("SearchDb",
+            MongoClientSettings.FromConnectionString(
+                builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(i => i.Make, KeyType.Text)
+            .Key(i => i.Model, KeyType.Text)
+            .Key(i => i.Color, KeyType.Text)
+            .CreateAsync();
+
+        await app.RunAsync();
     }
 }
